f0ae3ab6852c9278821e26c819c3fd38dc6ccd93
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-02-08 12:19:22 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react'
+
 
 function App() {
   
-
   return (
     <>
-      Hello World
+      <h1 className='text-5xl text-red-500 font-bold'>Hello World</h1>
     </>
   )
 }
