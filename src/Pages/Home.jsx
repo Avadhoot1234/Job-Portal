8a403c4eba9c49ab34921a6a57504a7f87000c48
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-02-13 13:02:21 +0530

    Done with the banner

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Banner from '../components/Banner'
 
 const Home = () => {
+  //Handling the search bar with the usestate
+      const [query,setQuery]=useState("");
+      const handleInputChange=(e)=>{
+          setQuery(e.target.value);
+          console.log(e.target.value)
+      }  
   return (
     <div>
-      Home
+      <Banner query={query} handleInputChange={handleInputChange}/>
     </div>
   )
 }
