26dd1d7668f929ee7769e312275cbe7e8340ca0c
Avadhoot1234 <avadhoot.dhumal9@gmail.com>
2025-02-21 23:39:14 +0530

    Sidebar and other stuff

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,12 +8,16 @@ const Home = () => {
 
   const[selectedCategory,setSelectedCategory]=useState(null);
   const[jobs,setJobs]=useState([]);
+  const [isLoading,setIsLoading]=useState(true);
+  const [currentPage,setCurrentPage]=useState(1);
+  const itemsPerPage=6;
 
   //Fetching data from the json file
   useEffect(()=>{
     fetch("jobs.json").then(res=>res.json()).then(data=>{
       // console.log(data);
-      setJobs(data)
+      setJobs(data);
+      setIsLoading(false);
     })
   },[])
 
@@ -42,6 +46,26 @@ const Home = () => {
     setSelectedCategory(event.target.value);
   }
 
+  //calculate the index range
+  const calculatePageRange=()=>{
+    const startIndex=(currentPage-1)*itemsPerPage;
+    const endIndex=startIndex+itemsPerPage;
+    return {startIndex,endIndex};
+  }
+
+  //function for the next page
+  const nextPage=()=>{
+    if(currentPage<Math.ceil(filteredItems.length/itemsPerPage)){
+      setCurrentPage(currentPage+1);
+    }
+  }
+
+  const prevPage=()=>{
+    if(currentPage<Math.ceil(filteredItems.length/itemsPerPage)){
+      setCurrentPage(currentPage-1);
+    }
+  } 
+
   //main function
   const filteredData=(jobs,selected,query)=>{
     let filteredJobs=jobs;
@@ -60,9 +84,11 @@ const Home = () => {
         salaryType.toLowerCase()===selected.toLowerCase()||
         employmentType.toLowerCase()===selected.toLowerCase()
       ))
-      console.log(filteredJobs);
+      // console.log(filteredJobs);
     }
 
+    const{startIndex,endIndex}=calculatePageRange();
+    filteredJobs=filteredJobs.slice(startIndex,endIndex)
     return filteredJobs.map((data,i)=><Card key={i} data={data}/>)
 
   }
@@ -80,7 +106,28 @@ const Home = () => {
         </div>
 
         {/* Main Job Cards */}
-        <div className='col-span-2 bg-white p-4 rounded-sm'><Jobs result={result}/></div>
+        <div className='col-span-2 bg-white p-4 rounded-sm'>
+          
+          {
+            isLoading ? (<p className='font-medium'>Loading...</p>): result.length>0?<Jobs result={result}/>:<>
+            <h3 className='text-lg font-bold mb-2'>{result.length} Jobs</h3>
+            <p>No Job Posting</p>
+            </>
+          }
+          
+
+          {/* Pagination */}
+          {
+            result.length> 0 ?(
+              <div className='flex justify-center mt-4 space-x-8'>
+                <button onClick={prevPage}>Previous</button>
+                <span>Page {currentPage} of {Math.ceil(filteredItems.length/itemsPerPage)}</span>
+                <button onClick={nextPage}>Next</button>
+              </div>
+            ):""
+          }
+        
+        </div>
 
         {/* Right side of the content */}
         <div className='bg-white p-4 rounded'>Right</div>
--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import localname from './Locution'
 import Locution from './Locution'
+import Salary from './Salary'
 
 const Sidebar = ({handleChange,handleClick}) => {
   return (
@@ -8,7 +9,7 @@ const Sidebar = ({handleChange,handleClick}) => {
       <h3 className='text-lg font-bold mb-2'>Filters</h3>
 
         <Locution handleChange={handleChange}/>
-
+        <Salary handleChange={handleChange} handleClick={handleClick}/>
     </div>
   )
 }
